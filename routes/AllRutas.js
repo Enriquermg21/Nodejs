bb01cd3dca41aad7ccb6be439c16b15fecb0a46c
Juanto4 <juantorresgallego@gmail.com>
2023-05-23 11:09:15 +0200

    Rutas crud

--- routes/AllRutas.js
+++ routes/AllRutas.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const autor = require("../controllers/signUp_signIn");
-/*const crudAlumnos = require('../crudAlumno/crudAlumno');
-const crudProfesor = require('../crudProfesor/crudProfesor');
-const crudClases = require('../crudClases/crudClases');
-const crudCurso = require('../crudCurso/crudCurso');
-const crudPadre = require('../crudPadre/crudPadre');
-const userPdf_Html = require("../Pdf_Html/Pdf_Html");
+const crudArticulos = require('../Cruds/crudArticulo/crudAlumno');
+const crudCliente = require('../Cruds/crudCliente/crudCliente');
+const crudFGastos = require('../Cruds/crudFGastos/crudFGastos');
+const crudFamilia = require('../Cruds/crudFamilia/crudFamilia');
+const crudProveedores = require('../Cruds/crudProveedores/crudProveedores');
 
-//Ruta para hacer un Pdf y un HTML
-router.use("/",userPdf_Html)
 
 //Rutas para Crud
-router.use("/",crudAlumnos)
-router.use("/",crudProfesor)
-router.use("/",crudClases)
-router.use("/",crudCurso)
-router.use("/",crudPadre)
+router.use("/",crudArticulos)
+router.use("/",crudCliente)
+router.use("/",crudFGastos)
+router.use("/",crudFamilia)
+router.use("/",crudProveedores)
 
 //Rutas para autorizacion
 router.post('/api/autorizacion/signup',autor.signUp)
