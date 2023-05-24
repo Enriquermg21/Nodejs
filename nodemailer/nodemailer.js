e17ad6244f7679d65d9bb4cc89159ffaa7ae9f72
Enrique <enriquermg21@gmail.com>
2023-05-24 10:38:08 +0200

    .

--- nodemailer/nodemailer.js
+++ nodemailer/nodemailer.js
@@ -1,33 +1,37 @@
 const nodemailer = require('nodemailer');
 const Usuario = require("../modelos/usuarios");
 const jwt = require('jsonwebtoken');
-const Role = require("../modelos/roles");
-const CLIENTD_ID = "";
-const CLIENT_SECRET = "";
-const REDIRECT_URI = "";
-const REFRESH_TOKEN = "";
-CONST OAUTH2CLIENT = 
+const { google } = require('googleapis');
+const CLIENTD_ID = "975928472019-tpgpvt9dlcoau2rmdf13ubh6b71ec2n0.apps.googleusercontent.com";
+const CLIENT_SECRET = "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";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN = "1//04nqsPClQae2eCgYIARAAGAQSNwF-L9IrEIyVrNTd5dRLG9VASFoB6DV7pOCeFZ3-mEcFvxWd5Fz1FMp5E3oJUsyz2O4eFI1imcg";
+const oAuth2Client =  new google.auth.OAuth2(
+  CLIENTD_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI,
+  REFRESH_TOKEN
+);
+oAuth2Client.setCredentials({refresh_token : REFRESH_TOKEN})
 
-async function enviarCorreoRestablecerContraseña(email) {
+async function enviarCorreoRestablecerContraseña() {
   try {
-    const usuario = await Usuario.findOne({ Email: email });
-
-    if (!usuario) {
-      throw new Error('Usuario no encontrado');
-    }
-
-    const token = jwt.sign({ id: usuario._id }, 'secreto', { expiresIn: '15m' });
+    const usuario = await Usuario.findOne({ Email:email });
+    const accessToken=await oAuth2Client.getAccessToken()
     // Enviar correo electrónico con el enlace de restablecimiento de contraseña
     const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        auth: {
-          user: 'dam2fct@gmail.com',
-          pass: 'dam2fctfran'
-        }
+      service:"gmail",
+      auth:{
+        type:"OAuth2",
+        user:"dam2fct@gmail.com",
+        clientId:CLIENTD_ID,
+        clientSecret:CLIENT_SECRET,
+        refresh_Token:REFRESH_TOKEN,
+        accessToken:accessToken
+      }
+      
     });
-
+    const token = jwt.sign({ id: usuario._id }, 'secreto', { expiresIn: '15m' });
     const mailOptions = {
       from: 'dam2fct@gmail.com',
       to: 'dam2fct@gmail.com',
@@ -38,14 +42,23 @@ async function enviarCorreoRestablecerContraseña(email) {
         <a href="http://tu_aplicacion.com/restablecer-contraseña/${token}">Restablecer contraseña</a>
       `
     };
-
-    const info = await transporter.sendMail(mailOptions);
+    
+    if (!usuario) {
+      throw new Error('Usuario no encontrado');
+    }
+    
+    const info = await transporter.enviarCorreoRestablecerContraseña(mailOptions);
     console.log('Correo electrónico enviado', info.response);
-    return { success: true };
+    return {info};
+
   } catch (error) {
     console.log('Error al enviar el correo electrónico', error);
     throw new Error('Error al enviar el correo electrónico');
   }
 }
+enviarCorreoRestablecerContraseña()
+  .then((result)=>result.status(200).send("enviado"))
+  .catch((error)=>console.log(error.message));
+
 
 module.exports = enviarCorreoRestablecerContraseña;
