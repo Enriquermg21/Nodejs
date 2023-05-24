3d010fefa242de0420da352a5e30549cb10ef131
Enrique <enriquermg21@gmail.com>
2023-05-24 13:46:50 +0200

    .

--- Restablecer/RestablecerPassword.js
+++ Restablecer/RestablecerPassword.js
@@ -1,30 +1,37 @@
 const express = require('express');
 const User = require('../modelos/usuarios');
 const bcrypt = require('bcrypt');
-const router = express.Router();
+const jwt = require('jsonwebtoken');
 
-  router.patch('/users/password', async (req, res) => {
-  const { email, password } = req.body;
+const router = express.Router();
+router.patch('/users/password', async (req, res) => {
+  const { token, password } = req.body;
 
   try {
-    // Buscar al usuario en la base de datos por su email
-    const user = await User.findOne({ Email:email });
+    console.log('Token recibido:', token);
+
+    // Verificar y decodificar el token de restablecimiento de contraseña
+    const decodedToken = jwt.verify(token, 'secreto', { expiresIn: '10s' });
+    const userId = decodedToken.id;
+
+    // Buscar al usuario en la base de datos por su ID
+    const user = await User.findById(userId);
     if (!user) {
       return res.status(404).json({ error: 'Usuario no encontrado.' });
     }
-    // Verificar si la contraseña actual proporcionada coincide con la contraseña almacenada
-    const esIgual =  bcrypt.compare(password, user.Password);
-    if (!esIgual) {
-      return res.status(401).json({ error: 'La contraseña actual es igual a la anterior.' });
-    }
+
     // Encriptar la nueva contraseña antes de guardarla en la base de datos
     const hashedPassword = await bcrypt.hash(password, 10);
-    
+
     // Actualizar la contraseña del usuario
     user.Password = hashedPassword;
     await user.save();
+
     res.json({ message: 'Contraseña cambiada exitosamente.' });
   } catch (error) {
+    if (error.name === 'TokenExpiredError') {
+      return res.status(401).json({ error: 'El token de restablecimiento de contraseña ha expirado.' });
+    }
     console.error(error);
     res.status(500).json({ error: 'Ocurrió un error al cambiar la contraseña del usuario.' });
   }
--- nodemailer/nodemailer.js
+++ nodemailer/nodemailer.js
@@ -1,50 +1,52 @@
-let enlaceHabilitado = true;
 const nodemailer = require('nodemailer');
 const Usuario = require("../modelos/usuarios");
 const { google } = require('googleapis');
-const CLIENTD_ID = "975928472019-tpgpvt9dlcoau2rmdf13ubh6b71ec2n0.apps.googleusercontent.com";
+const jwt = require('jsonwebtoken');
+
+const CLIENT_ID = "975928472019-tpgpvt9dlcoau2rmdf13ubh6b71ec2n0.apps.googleusercontent.com";
 const CLIENT_SECRET = "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";
 const REDIRECT_URI = "https://developers.google.com/oauthplayground";
 const REFRESH_TOKEN = "1//04nqsPClQae2eCgYIARAAGAQSNwF-L9IrEIyVrNTd5dRLG9VASFoB6DV7pOCeFZ3-mEcFvxWd5Fz1FMp5E3oJUsyz2O4eFI1imcg";
-const oAuth2Client =  new google.auth.OAuth2(
-  CLIENTD_ID,
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
   CLIENT_SECRET,
-  REDIRECT_URI,
-  REFRESH_TOKEN
+  REDIRECT_URI
 );
-oAuth2Client.setCredentials({refresh_token : REFRESH_TOKEN})
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
 async function sendMail(email) {
-  const usuario = await Usuario.findOne({ Email:email });
-  
+  const usuario = await Usuario.findOne({ Email: email });
+
   if (!usuario) {
     throw new Error('Usuario no encontrado');
   }
+
   try {
-    const accessToken=await oAuth2Client.getAccessToken()
+    const token = jwt.sign({ id: usuario._id }, 'secreto', { expiresIn: '10s' });
+    const accessToken = await oAuth2Client.getAccessToken();
     const transporter = nodemailer.createTransport({
-      service:"gmail",
-      auth:{
-        type:"OAuth2",
-        user:"dam2fct@gmail.com",
-        clientId:CLIENTD_ID,
-        clientSecret:CLIENT_SECRET,
-        refreshToken:REFRESH_TOKEN,
-        accessToken:accessToken
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: "dam2fct@gmail.com",
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        refreshToken: REFRESH_TOKEN,
+        accessToken: accessToken
       }
-      
     });
     const mailOptions = {
       from: 'dam2fct@gmail.com',
       to: usuario.Email,
       subject: 'Restablecimiento de contraseña',
-      html: 
-        `<p>Hola ${usuario.Nombre},</p>
+      html: `
+        <p>Hola ${usuario.Nombre},</p>
         <p>Has solicitado restablecer tu contraseña. Haz clic en el siguiente enlace para cambiarla:</p>
-        <a id="resetLink" href="http://localhost:3004/users/password">Restablecer contraseña</a>
+        <p>EL TOKEN EXPIRA EN 10s</p>
+        <a id="resetLink" href="http://localhost:3004/users/password?token=${token}">Restablecer contraseña</a>
       `
     };
-    
+
     const info = await transporter.sendMail(mailOptions);
     console.log('Correo electrónico enviado');
 
