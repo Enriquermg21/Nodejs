9e168bd9960df6635461fbb2874db49b906db9cc
Enrique <enriquermg21@gmail.com>
2023-06-01 10:52:11 +0200

    .

---
+++ blog-mern-app-main/blog-mern-app-main/blog/dbconfig/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const db_uri = 'mongodb+srv://enriquermg21:5zcLF6CKdi4OdqEK@cluster0.qu92wqz.mongodb.net/Blog'
+
+module.exports = ()=>{
+    const connect = ()=>{
+        mongoose.connect(db_uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+          })
+            .then(() => {
+              console.log('Conectado a la base de datos');
+            })
+            .catch((error) => {
+              console.log('Error de conexión: ' + error.message);
+            });
+    }
+    connect();
+}
---
+++ blog-mern-app-main/blog-mern-app-main/blog/middleware/verifyIs.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const Usuario = require('../models/Usuario');
+
+const verifyToken = async(req,res,next)=>{
+    try{let token = req.headers['token'] || req.headers['authorization'];
+    if (!token) return res.status(403).json({message: "No se ha recibido ningun token"})
+    if(token.startsWith('Bearer ')){
+      token = token.slice(7,token.length);
+    }
+    const decoded = jwt.verify(token,"instituto-api")
+    req.id = decoded.id;
+    const Usuar = await Usuario.findById(req.id,{password:0})
+    if (!Usuar) return res.status(403).json({message: "No se ha encontrado usuario"})
+
+    next()}
+    catch(error){
+        return res.status(403).json({message: "No autorizado"})
+    }
+    
+}
+module.exports = { verifyToken,routeDuration};
