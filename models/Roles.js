f1b971a1d03dcc41816864f0ef1e81c37b8de2c9
Enrique <enriquermg21@gmail.com>
2023-06-01 10:29:38 +0200

    .

--- models/Roles.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const roleSchema = new mongoose.Schema({
-      Rol: {
-        type: String
-      }
-    },{ versionKey: false });
-const Role = mongoose.model('Role', roleSchema);
-module.exports= Role;
